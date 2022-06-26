c51a647c9937c6a3ece812c65278788dd9fdb15c
ahidalgoj <77654282+ahidalgoj@users.noreply.github.com>
2022-06-26 13:47:23 -0600

    Revert "Add dropwon list"

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
 
 import React from 'react';
 import { View } from 'react-native';
-import { FightScreen } from './src/screens/FightScreen';
 
 
 export const App = () => {
   return (
-    <View>
-      <FightScreen/>
-    </View>
+    <View></View>
   )
 }
